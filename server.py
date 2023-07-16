3889a312571025888dd00ec7cf379a34bd4c2960
RafDevOfficial <83568542+RafCodes-del@users.noreply.github.com>
2023-07-16 12:07:24 +0800

    Update server.py

--- server.py
+++ server.py
@@ -1,31 +1,36 @@
-#copyright rafstudios
-#Server For NetExecute
 import socket
 
-HOST = 'localhost'
-PORT = 8080
+# Define the server host and port
+HOST = 'localhost'  # Change this to your server IP or hostname
+PORT = 12345  # Choose any available port number
 
-def handle_command(command):
-    if command.startswith("display"):
-        text = command.split("[")[1].split("]")[0]
-        return f"Displaying: {text}"
-    else:
-        return "Invalid command"
+# Create a socket object
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-def start_server():
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind((HOST, PORT))
-        s.listen(1)
-        print(f"Server listening on {HOST}:{PORT}")
-        conn, addr = s.accept()
-        with conn:
-            print("Connected by", addr)
-            while True:
-                data = conn.recv(1024)
-                if not data:
-                    break
-                command = data.decode()
-                response = handle_command(command)
-                conn.sendall(response.encode())
+# Bind the socket to the host and port
+server_socket.bind((HOST, PORT))
 
-start_server()
+# Listen for incoming connections
+server_socket.listen(1)
+
+print('Server listening on', HOST, PORT)
+
+# Accept a client connection
+client_socket, client_address = server_socket.accept()
+print('Client connected:', client_address)
+
+# Read commands from a file
+with open('accessclient.txt', 'r') as f:
+    commands = f.readlines()
+
+# Process and send commands to the client
+for command in commands:
+    command = command.strip()  # Remove leading/trailing whitespace
+
+    # Send the command to the client
+    client_socket.sendall(command.encode())
+    print('Sent command:', command)
+
+# Close the client connection
+client_socket.close()
+server_socket.close()
